c9c0a3be11e86ddba3f86a6e068c8712a412d436
yuuki <84gn70g@gmail.com>
2018-06-30 23:52:55 +0900

    test

--- src/test.js
+++ src/test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 //コンポーネント名は大文字で始める
+//コンポーネントの引数名はpropsにする
 const Test = (props) => {
   const Name = (props) => {
     return (
@@ -12,13 +13,22 @@ const Test = (props) => {
   }
 
   const age = () => <p>age</p>
-
+  const address = (str) => {
+    return (
+      <div>
+        <p>Address</p>
+        <p>{str}</p>
+        
+      </div>
+    )
+  }
   return (
     <div>
       <p>React Sample</p>
-      <Name str="aa" />
-      {Name({ a: "bb" })}
+      <Name str="mm"/>
+      {Name({ str: "xx" })}
       {age()}
+      {address("Tokyo")}
     </div>
   )
 }
